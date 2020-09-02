bdebc5cfe0342d598ddacbe906b76399db92a5f9
Heeryon Cho <endeavor6@naver.com>
2020-09-03 06:43:08 +0900

    revise compute_OKS_rescale.py

--- Lab/chr/compute_OKS_rescale.py
+++ Lab/chr/compute_OKS_rescale.py
@@ -4,16 +4,21 @@ import json
 from scipy.spatial import distance
 import math
 
+'''
+Pose Estimation. Metrics. by Alexander Stasiuk
+https://medium.com/@masherov14/pose-estimation-metrics-844c07ba0a78
+'''
+
 # json파일 폴더에서 180개의 json파일을 읽어 (x, y) tuple이 들어있는 4500 (25 keypoints * 180 frame) 벡터 추출
 def read_vec(json_path):
     json_path = json_path
-    json_files = [f for f in listdir(json_path) if isfile(join(json_path, f))]
-    json_files.sort()
+    keypoint_data_jsons = [f for f in listdir(json_path) if isfile(join(json_path, f))]
+    keypoint_data_jsons.sort()
 
     keypoint_data = []
-    for json_file in json_files:
-        #print(json_file)
-        with open(json_path+json_file) as f:
+    for keypoint_data_json in keypoint_data_jsons:
+        #print(keypoint_data_json)
+        with open(json_path+keypoint_data_json) as f:
             data = json.load(f)
         ary = data["people"][0]["pose_keypoints_2d"]
         del ary[2::3]
@@ -34,7 +39,7 @@ def read_vec(json_path):
 
     return keypoint_data
 
-# PDJ (Percentage of Detected Joints) 계산식 중 diagonal에 해당하는 값을 base (length between nose and middle hip)의 형태로 구함
+# 사람 기준 길이의 diagonal에 해당하는 값을 base (length between nose and middle hip)의 형태로 구함
 def get_base(v1, v2):
     # 스승과 학생의 base 길이를 각각 구함
     base1 = distance.euclidean(v1[0], v1[8]) # 스승
@@ -43,8 +48,8 @@ def get_base(v1, v2):
     scaling_factor = base1 / base2
     
     # 두 base의 평균을 기준 길이(PDJ의 diagonal)로 정함
-    result = (base1 + base2) / 2
-    return scaling_factor, result
+    base = (base1 + base2) / 2
+    return scaling_factor, base
 
 
 # OKS (Object Keypoint Similarity)를 계산
@@ -64,66 +69,29 @@ def compute_pdj(v1, v2, alpha):
     result = oks / len(v1)
     return result
 
+path_tt = "keypoint_data_json/teacher_tadasan_json/content/openpose/output/"
+path_st = "keypoint_data_json/student_tadasan_json/content/openpose/output/"
 
-path_at = "json_file/ameya_tadasan/ameya_tadasan_json/content/openpose/output/"
-path_kt = "json_file/kaustuk_tadasan/kaustuk_tadasan_json/content/openpose/output/"
+path_tv = "keypoint_data_json/teacher_vriksh_json/content/openpose/output/"
+path_sv = "keypoint_data_json/student_vriksh_json/content/openpose/output/"
 
-path_av = "json_file/ameya_vriksh/ameya_vriksh_json/content/openpose/output/"
-path_kv = "json_file/kaustuk_vriksh/kaustuk_vriksh_json/content/openpose/output/"
+tt = read_vec(path_tt)
+print(len(tt))
 
-at = read_vec(path_at)
-print(len(at))
-#print(at)
+st = read_vec(path_st)
+print(len(st))
 
-kt = read_vec(path_kt)
-print(len(kt))
+tv = read_vec(path_tv)
+print(len(tv))
 
-av = read_vec(path_av)
-print(len(av))
+sv = read_vec(path_sv)
+print(len(sv))
 
-kv = read_vec(path_kv)
-print(len(kv))
+alpha = 0.10
 
-alpha = 0.05
-alpha = 0.08
-alpha = 0.1
-alpha = 0.02
+print("teacher_tadasan vs. student_tadasan:", compute_pdj(tt, st, alpha))
+print("teacher_vriksh vs. student_vriksh:", compute_pdj(tv, sv, alpha))
+print("")
+print("teacher_tadasan vs. student_vriksh:", compute_pdj(tt, sv, alpha))
+print("teacher_vriksh vs. student_tadasan:", compute_pdj(tv, st, alpha))
 
-print("ameya_tadasan vs. kaustuk_tadasan:", compute_pdj(at, kt, alpha))
-print("ameya_vriksh vs. kaustuk_vriksh:", compute_pdj(av, kv, alpha))
-
-print("ameya_tadasan vs. kaustuk_vriksh:", compute_pdj(at, kv, alpha))
-print("ameya_vriksh vs. kaustuk_tadasan:", compute_pdj(av, kt, alpha))
-
-
-'''
-
-# base = 0.05
-
-ameya_tadasan vs. kaustuk_tadasan: 0.50897010604838
-ameya_vriksh vs. kaustuk_vriksh: 0.5043158743768617
-ameya_tadasan vs. kaustuk_vriksh: 0.22397680495947056
-ameya_vriksh vs. kaustuk_tadasan: 0.23015459352577575
-
-# base = 0.08
-
-ameya_tadasan vs. kaustuk_tadasan: 0.6480285209685896
-ameya_vriksh vs. kaustuk_vriksh: 0.6397774888054402
-ameya_tadasan vs. kaustuk_vriksh: 0.31684365658826613
-ameya_vriksh vs. kaustuk_tadasan: 0.31071593625167054
-
-# base = 0.1
-
-ameya_tadasan vs. kaustuk_tadasan: 0.7199944459983096
-ameya_vriksh vs. kaustuk_vriksh: 0.7047415191846264
-ameya_tadasan vs. kaustuk_vriksh: 0.3715156374801867
-ameya_vriksh vs. kaustuk_tadasan: 0.36206180439596236
-
-# base = 0.02
-
-ameya_tadasan vs. kaustuk_tadasan: 0.2542515034621694
-ameya_vriksh vs. kaustuk_vriksh: 0.2750122432503172
-ameya_tadasan vs. kaustuk_vriksh: 0.10854828416769136
-ameya_vriksh vs. kaustuk_tadasan: 0.15401851596608568
-
-'''
